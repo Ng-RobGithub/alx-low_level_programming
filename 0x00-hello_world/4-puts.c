0019bbe3a7e11dedc500e7af6804d79a0a67c9a1
Ng-RobGithub <ngozi.agomuonso@gmail.com>
2023-02-16 17:24:53 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#Include <stdio.h>
+/**
+ * Main A program that print a line with puts function
+ * return: 0(success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
