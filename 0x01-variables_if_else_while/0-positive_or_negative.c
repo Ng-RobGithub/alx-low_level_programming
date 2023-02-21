f695843769741c3c7ef46374398ab2e9f39e9951
Ng-RobGithub <ngozi.agomuonso@gmail.com>
2023-02-20 18:28:05 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%d is Positive\n", n);
+	}
 	else if (n < 0)
+	{
 		printf("%d is Negative\n", n);
+	}
 	else
+	{
 		printf("%d is Zero\n", n);
+	}
 	return (0);
 }
