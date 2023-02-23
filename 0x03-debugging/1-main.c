a4b81a010cb9f97a3e356a4a81e1c089b0b60db2
Ng-RobGithub <ngozi.agomuonso@gmail.com>
2023-02-23 07:30:39 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - causes an infinite loop
- *Return: 0
+ * main - causes an infinite loop
+ * Return: 0
  */
-
 int main(void)
 {
 	int i;
@@ -18,6 +17,5 @@ int main(void)
 	putchar(i);
 	}
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
