a064825df77f5da8295029341b50c90f29411f02
Ng-RobGithub <ngozi.agomuonso@gmail.com>
2023-02-26 13:57:34 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * more_nummers - a function that prints 10 times the numbers,
+ * more_numbers - A function that prints 10 times the numbers,
  * from 0 to 14, followed by a new line
  *
- * Return:
+ * Return: void
  */
 void more_numbers(void)
 {
