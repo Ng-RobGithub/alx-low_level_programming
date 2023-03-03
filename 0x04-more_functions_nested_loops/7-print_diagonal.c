7f04194c0132bb7478aa35e66d1f280163a62339
Ng-RobGithub <ngozi.agomuonso@gmail.com>
2023-03-03 14:58:21 -0800

    commit diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,30 @@
 #include "main.h"
 
 /**
- * print_diagonal - A function that draws a diagonal line on the terminal.
- *
- * @n: the number of diagonal to draw
- * Return: void
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
  */
+
 void print_diagonal(int n)
 {
-	int d, g;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-		for (d = 0; d <= n; d++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (g = 0; g < d; g++)
+			for (j = 0; j < n; j++)
 			{
-				_putchar(' ');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
-			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar ('\n');
 	}
 }
+
