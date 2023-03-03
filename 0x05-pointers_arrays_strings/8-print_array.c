b1940998fbe723a61856a6eb3f2ed76383323ee5
Ng-RobGithub <ngozi.agomuonso@gmail.com>
2023-03-03 13:54:27 -0800

    commit no.8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,20 +2,22 @@
 #include <stdio.h>
 
 /**
- * print_array - a function that prints n elements of an array of integers,
- *
- * @a: an array
- * @n: number of values to be printed
+ * print_array - a function that prints n elements of an array
+ * @a: array name
+ * @n: is the number of elements OF the array to be printed
+ * Return: a and n inputs
  */
 void print_array(int *a, int n)
 {
-	int g;
+	int i;
 
-	for (g = 0; g < n; g++)
+	for (i = 0; i < (n - 1); i++)
 	{
-		printf("%d", a[g]);
-		if (g != n - 1)
-		printf(" , ");
+		printf("%d, ", a[i]);
+	}
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
 	}
 	printf("\n");
 }
