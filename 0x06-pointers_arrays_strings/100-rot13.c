b42b2e229d0bcc01f7fccccc66aa5f8d29fbc12e
Ng-RobGithub <ngozi.agomuonso@gmail.com>
2023-03-03 02:30:07 -0800

    commit all

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,21 +3,22 @@
 
 /**
  * rot13 - a function that encodes a string using rot13
- *@s: pointer to string
+ *@s: pointer to string params
  *
  * Return: *s
  */
 char *rot13(char *s)
 {
-	int i, j;
-	char datal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == datal[j])
+			if (s[i] == data1[j])
 			{
 				s[i] = datarot[j];
 				break;
