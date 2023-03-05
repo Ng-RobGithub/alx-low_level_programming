d9615fe94059a340f67f174678ff1cbbe62ba8c4
Ng-RobGithub <ngozi.agomuonso@gmail.com>
2023-03-05 05:39:03 -0800

    commit task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,27 @@
-#include "main.h"
+# include "main.h"
 
 /**
- * print_sign - A function that prints the sign of a number.
- * @n: The number of sign to be printed
- * Return: 1 if the number is greater than zero
- * 0 if the number is zero
- * -1 if the number is less than zero
+ * print_sign-check the sign of a number.
+ *
+ * @n:Number whose sign is to be checked.
+ * Return:1-if n is greater than 0: 0- if n equals 0:-1-if n is less than 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		putchar('0');
-		return (0);
+		_putchar ('-');
+		return (-1);
 	}
 	else
 	{
-		putchar('-');
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 }
 
