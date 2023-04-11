b4b2c16ede4adb3bb68a0853aae321f06e0f9b8f
Ng-RobGithub <ngrob4real@gmail.com>
2023-04-11 15:53:10 -0700

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int fn;
 	int nlet;
-	int nWR;
+	int rWR;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -20,13 +20,14 @@ int create_file(const char *filename, char *text_content)
 	if (fn == -1)
 		return (-1);
 
-	if (text_content)
+	if (!text_content)
 		text_content = "";
+
 	for (nlet = 0; text_content[nlet]; nlet++)
 
-	nWR = write(fn, text_content, nlet);
+	rWR = write(fn, text_content, nlet);
 
-	if (nWR == -1)
+	if (rWR == -1)
 		return (-1);
 
 	close(fn);
