742622b49539700d70dfc41668aaa040c20f5c83
Ng-RobGithub <ngrob4real@gmail.com>
2023-05-08 14:36:35 -0700

    A function that create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int nlet;
 	int rWR;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (fn == -1)
 		return (-1);
 
-	if (text_content)
+	if (!text_content)
 		text_content = "";
 
 	for (nlet = 0; text_content[nlet]; nlet++)
