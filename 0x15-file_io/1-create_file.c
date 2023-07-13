322fca1f7109c105910c1325749f5d4c215e1dbe
Ng-RobGithub <ngrob4real@gmail.com>
2023-07-13 00:00:11 -0700

    A function that create a function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -9,27 +8,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fn;
-	int nlet;
-	int rWR;
+	int fd;
+	int result;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
-	fn = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
-	if (fn == -1)
-		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (nlet = 0; text_content[nlet]; nlet++)
-
-		rWR = write(fn, text_content, nlet);
-
-	if (rWR == -1)
+	if (fd == -1)
 		return (-1);
 
-	close(fn);
-	return (1);
+	if (text_content != NULL)
+	{
+		result = write(fd, text_content, strlen(text_content));
+
+		if (result == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	    close(fd);
+	    return (1);
 }
