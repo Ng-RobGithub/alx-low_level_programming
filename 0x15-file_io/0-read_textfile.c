1fc2391201defd54575b186bf6b9d3fe74036a0d
ngrob4real@gmail.com <Ng-RobGithub>
2023-08-07 14:57:39 -0700

    a function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile- a function that reads a text file and
+ * prints it to the POSIX standard output.
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: Nos. of letters printed on success, 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	char *buf;
+	int fn;
+	ssize_t nRD;
+	ssize_t nWR;
+
+	if (!filename)
+		return (0);
+
+	fn = open(filename, O_RDONLY);
+
+	if (fn == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nRD = read(fn, buf, letters);
+	nWR = write(STDOUT_FILENO, buf, nRD);
+
+	close(fn);
+
+	free(buf);
+
+	return (nWR);
+}
