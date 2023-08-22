f7b4a98b5abd4b0b57d316370c86025999d0c5f0
Ng-RobGithub <ngrob4real@gmail.com>
2023-08-22 15:25:35 -0700

    a dynamic library that contains C functions that can be called from Python.

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * greet - Prints a greeting message
+ */
+void greet(void)
+{
+	printf("Hello from C function!\n");
+}
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/100-operations.h
@@ -0,0 +1,9 @@
+#ifndef _100_OPERATIONS_H_
+#define _100_OPERATIONS_H_
+
+#include <stdio.h>
+
+void greet(void);
+int add(int a, int b);
+
+#endif /* _100_OPERATIONS_H_ */
