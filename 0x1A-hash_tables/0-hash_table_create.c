58fb0480aeecfbd3e4f35227c13e17fe2fb64d10
Ng-RobGithub <ngrob4real@gmail.com>
2023-09-10 14:45:36 -0700

    Update:a function that creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,38 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
+ * hash_table_create - Creates a new hash table.
+ * @size: The size of the array in the hash table.
  *
- * Return: If an error occurs - NULL.
- * Otherwise - a pointer to the new hash table.
+ * Return: A pointer to the newly created hash table,
+ * or NULL if an error occurs.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *new_table = NULL;
 	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (size < 1)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	/* Allocate memory for the hash table structure */
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
 		return (NULL);
+
+	/* Allocate memory for the array of hash nodes (buckets) */
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	/* Initialize the hash nodes to NULL */
 	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+		new_table->array[i] = NULL;
 
-	return (ht);
+	new_table->size = size;
+
+	return (new_table);
 }
